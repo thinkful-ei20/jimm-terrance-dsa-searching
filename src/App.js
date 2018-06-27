25cabaa45d8692251df1f3dd49e789f09fff9f85
Terrance Corley <terrancecorley@outlook.com>
2018-06-27 09:35:24 -0700

    added search boilerplate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      output: 'press a button',
+      linearData: [89, 30, 25, 32, 72, 70],
+      whichBtn: null
+    };
+  }
+  
+  linearSearch(dataset, item) {
+    let count;
+    for (let i = 0; i < dataset.length; i++) {
+      let currItem = dataset[i];
+      count = i;
+      if (currItem === item) {
+        return currItem;
+      }
+    }
+    this.setState({output: `Found ${item}, Took ${count} Searches`});
+    return 'The number was not found';
+  }
+
+  binarySearch() {
+
+  }
+
+  // write submit func
+  //  checks name val of btn "submitted"
+  //  grabs val of textarea
+  //  stores val in state
+  //  runs the func
+
+  onFormSubmit(e, btnName) {
+    e.preventDefault();
+    console.log(btnName);
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form className="App" onSubmit={(event) => 
+        this.onFormSubmit(event, this.state.whichBtn)}>
+        <textarea></textarea>
+        <button type="submit" name="linear_search_btn" 
+          onClick={() => this.setState({whichBtn: 'linear'})}>Linear Search</button>
+        <button type="submit" name="binary_search_btn" 
+          onClick={() => this.setState({whichBtn: 'binary'})}>Binary Search</button>
+        <p>Output: {this.state.output}</p>
+      </form>
     );
   }
 }
